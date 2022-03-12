011fd2444030377cee986e8fb148a3de11d9e779
artem <zalivonka02@gmail.com>
2022-03-12 15:03:57 +0300

    create library

--- book/models.py
+++ book/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField('название книги', max_length=50)
+    picture = models.ImageField('изображение')
+    author = models.CharField('автор', max_length=30, default='anonymous')
+    email = models.EmailField('email', blank=True)
+    describe = models.TextField('описание', default='Django tutorial')
+
+    def __str__(self):
+        return self.name
